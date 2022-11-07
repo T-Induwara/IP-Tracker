da3976a8cda4999e4ae04c4714064431f6331c2c
Tharusha Induwara <tharusha.induwar@gmail.com>
2022-11-07 12:10:50 +0530

    Added leaflet js with open street map

---
+++ script/script.js
@@ -0,0 +1,11 @@
+console.log("Script loaded");
+
+var map = L.map('map').setView([6.9271, 79.8612], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([6.9271, 79.8612]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .closePopup();
