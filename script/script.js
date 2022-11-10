c24fd127805c6d79700704c194ad4f6dd86c79e5
Tharusha Induwara <tharusha.induwar@gmail.com>
2022-11-10 13:12:43 +0530

    Added js variables for html location info containers

--- script/script.js
+++ script/script.js
@@ -1,12 +1,26 @@
 console.log("Script loaded");
 
+//variable declarations page info containers
+var ipAdd = document.getElementById("ip-add");
+let ipBtn = document.getElementById("ip-comm-btn");
+
+let ipOutAdd = document.getElementById("desc-ip").innerHTML;
+let ipOutLoc = document.getElementById("desc-loc").innerHTML;
+let ipOutTz = document.getElementById("desc-tz").innerHTML;
+let ipOutIsp = document.getElementById("desc-isp").innerHTML;
+
+ipBtn.addEventListener("click", function () {
+    console.log(ipAdd.value);
+});
+
 let locIcon = L.icon({
     iconUrl: "/images/icon-location.svg",//To change the default leaflet js marker icon
 })
 var mapOptions = {
     zoomControl: false,
 }
-var map = L.map('map', mapOptions).setView([6.9271, 79.8612], 16);
+
+/*var map = L.map('map', mapOptions).setView([6.9271, 79.8612], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -15,3 +29,13 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([6.9271, 79.8612], { icon: locIcon }).addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .closePopup();
+*/
+
+const map = L.map('map', {
+    'center': [42, 42],
+    'zoom': 16,
+    'layers': [L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })]
+})
+L.marker([6.9271, 79.8612], { icon: locIcon }).addTo(map);
