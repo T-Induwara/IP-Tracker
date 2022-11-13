48e0a403ea00b837796042ab34239a9ee28afc9c
Tharusha Induwara <tharusha.induwar@gmail.com>
2022-11-13 11:26:34 +0530

    Chcked ip address passing functionality to ipify api

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
 console.log("Script loaded");
 
 //variable declarations page info containers
-var apikay = 'at_tLaXMmat8Wh7kg4yNffBOfDX5jrv3';
+var apikey = 'at_tLaXMmat8Wh7kg4yNffBOfDX5jrv3';
+
+//var ip = "123.231.111.170";
+var api_key = "at_tLaXMmat8Wh7kg4yNffBOfDX5jrv3";
 
 var ipAdd = document.getElementById("ip-add");
 let ipBtn = document.getElementById("ip-comm-btn");
@@ -12,12 +15,14 @@ let ipOutTz = document.getElementById("desc-tz").innerHTML;
 let ipOutIsp = document.getElementById("desc-isp").innerHTML;
 
 ipBtn.addEventListener("click", function () {
-    if (ipAdd.value == "") {
-        alert("Please enter an IP address!!!");
-    }
-    else {
-        console.log(ipAdd.value);
-    }
+    $.ajax({
+        url: "https://geo.ipify.org/api/v1",
+        data: { apiKey: apikey, ipAddress: ipAdd.value },
+        success: function (data) {
+            console.log("GG");
+            $("body").append("<pre>" + JSON.stringify(data, "", 2) + "</pre>");
+        }
+    });
 });
 
 let locIcon = L.icon({
@@ -43,6 +48,7 @@ const map = L.map('map', {
     'zoom': 16,
     'zoomControl': false,//Zoom control buttons disabled
     'dragging': false,//Map movement by fingers or mouse disabled
+    'scrollWheelZoom': false,
     'layers': [L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     })]
