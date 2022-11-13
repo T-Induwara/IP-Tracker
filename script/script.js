c4b70b758588fb1ec0aa6551f53e7dc57a1d8f8b
Tharusha Induwara <tharusha.induwar@gmail.com>
2022-11-13 13:28:16 +0530

    Changed default zoom level from 16 to 15

--- script/script.js
+++ script/script.js
@@ -83,10 +83,10 @@ L.marker([6.9271, 79.8612], { icon: locIcon }).addTo(map)
 
 var map = L.map('map', {
     'center': [6.9271, 79.8612],
-    'zoom': 16,
+    'zoom': 15,
     'zoomControl': false,//Zoom control buttons disabled
-    //'dragging': false,//Map movement by fingers or mouse enabled
-    //'scrollWheelZoom': false,//Map scrolling by mouse enabled
+    'dragging': false,//Map movement by fingers or mouse enabled
+    'scrollWheelZoom': false,//Map scrolling by mouse enabled
     'layers': [L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     })]
